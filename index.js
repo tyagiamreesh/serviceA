347033382f4930b84334bb59b37281ea14bd5d7f
Amreesh Tyagi <amreesh.tyagi@monotype.com>
2018-09-14 11:37:15 +0530

    initial commit

--- index.js
+++ index.js
@@ -3,17 +3,17 @@ var os = require("os");
 const app = express();
 
 app.get('/', function(req, res) {
-  res.send('Hello, Welcome to demo app! at '+os.hostname());
+  res.send('Hello, Welcome to service A! at '+os.hostname());
 });
 
 app.get('/hello', function(req, res) {
-  res.send('Hello, Welcome to demo app! at '+os.hostname());
+  res.send('Hello, Welcome to service A! at '+os.hostname());
 });
 
 app.get('/hello/:name', function(req, res) {
-  res.send('Hello, ' + req.params.name + ' Welcome to demo app! at '+os.hostname());
+  res.send('Hello, ' + req.params.name + ' Welcome to service A! at '+os.hostname());
 });
 
 app.listen(5000, function () {
-  console.log('Server is running  at http://localhost:5000');
+  console.log('Service A is running  at http://localhost:5000');
 });
